0bb97e2506b7b31c51a3d4ad4e7858be03ab1ae9
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-05-14 13:01:16 +0800

    :heavy_minus_sign: remove dayjs marked q axios dependency.

--- src/global.js
+++ src/global.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import Vue from 'vue'
-import Filters from './filters'
 
 /* ------------------------Vue Global Config------------------------------ */
 Vue.config.productionTip = false
@@ -11,27 +10,20 @@ import { appTitle } from './config/constant'
 window.$appTitle = appTitle
 
 /* ------------------------Vue Global Variable------------------------------ */
-import { $apis, $utils, $document, $lodash } from '@helper'
+import { $utils, $document, $lodash } from '@helper'
 Vue.prototype.$_ = $lodash
-Vue.prototype.$apis = $apis
 Vue.prototype.$utils = $utils
 Vue.prototype.$document = $document
 
-for (let key in Filters) {
-  Vue.filter(key, Filters[key])
-}
-
 /* ------------------------Vue Global Components------------------------------ */
-import { Dropdown, DropdownMenu, DropdownItem } from 'element-ui'
+import { Dropdown, DropdownMenu, DropdownItem, Loading } from 'element-ui'
 Vue.use(Dropdown)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
+Vue.use(Loading)
 
 import VueMeta from 'vue-meta'
 Vue.use(VueMeta)
 
 import Icon from '@components/Icon'
 Vue.component('icon', Icon)
-
-import Arrow from '@components/icons/Arrow'
-Vue.component('arrow', Arrow)
--- src/helper/index.js
+++ src/helper/index.js
@@ -1,7 +1,5 @@
 /** @format */
 
-export const $ajax = require('./ajax').default
-export const $apis = require('./apis').default
 export const $utils = require('./utils').default
 export const $lodash = require('./lodash').default
 export const $document = require('./document').default
