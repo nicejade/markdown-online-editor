cff88b852e9aaf761bce490b01095d30f3d89449
杨琼璞 <jeffygisgreat@gmail.com>
2025-01-15 00:21:30 +0800

    ✨ Add canonical URL support for improved SEO in main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,23 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
+const updateCanonical = (url) => {
+  let link = document.querySelector("link[rel='canonical']")
+  if (!link) {
+    link = document.createElement('link')
+    link.setAttribute('rel', 'canonical')
+    document.head.appendChild(link)
+  }
+  link.setAttribute('href', url)
+}
+
+router.beforeEach((to, _, next) => {
+  const canonicalUrl = `${to.path}`
+  updateCanonical(canonicalUrl)
+  next()
+})
+
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
